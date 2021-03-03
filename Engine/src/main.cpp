28457df39d74e45707d9685cb7948546d9d17009
alsern01 <alsern01@ucm.es>
2021-03-03 12:51:29 +0100

    commit inicial con la estructura base del proyecto e instrucciones para compilar

---
+++ Engine/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <OgreRoot.h>
+
+using namespace Ogre;
+
+
+#if (defined _DEBUG) || !(defined _WIN32)
+int main() {
+
+#else
+#include <Windows.h>
+int WINAPI
+WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+
+	Root* root;
+
+#ifdef _DEBUG
+	root = new Root("OgreD/plugins.cfg");
+#else
+	root = new Root("Ogre/plugins.cfg");
+#endif
+
+
+	std::cout << "KEKW\n";
+
+	return 0;
+}
---
+++ bats/3_MoveDLLFiles.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo ---------------------------------------------------
+echo Moviendo .dll a bin
+echo ---------------------------------------------------
+
+::Main de OGRE
+echo moviendo OGRE
+copy ..\Engine\dependencies\Ogre\build\bin\debug\OgreMain_d.dll ..\Engine\bin
+copy ..\Engine\dependencies\Ogre\build\bin\release\OgreMain.dll ..\Engine\bin
+
+::zlib
+echo moviendo zlib
+copy ..\Engine\dependencies\Ogre\build\bin\debug\zlib.dll ..\Engine\bin
+copy ..\Engine\dependencies\Ogre\build\bin\release\zlib.dll ..\Engine\bin
+
+::Sistema de renderizado
+echo moviendo RenderSystem
+copy ..\Engine\dependencies\Ogre\build\bin\debug\RenderSystem_Direct3D11_d.dll ..\Engine\bin\OgreD
+copy ..\Engine\dependencies\Ogre\build\bin\release\RenderSystem_Direct3D11.dll ..\Engine\bin\Ogre
+
+::Para cargar imagenes .jpg, .png, etc
+echo moviendo CODEC
+copy ..\Engine\dependencies\Ogre\build\bin\debug\Codec_STBI_d.dll ..\Engine\bin\OgreD
+copy ..\Engine\dependencies\Ogre\build\bin\release\Codec_STBI.dll ..\Engine\bin\Ogre
+
+echo ---------------------------------------------------
+echo FIN
+echo ---------------------------------------------------
+
+PAUSE
