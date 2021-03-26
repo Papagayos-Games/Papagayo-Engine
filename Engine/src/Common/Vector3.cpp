a5a8c107c17e6c66c541ffb1297f3e066e1f2c83
aurora <aurgar01@ucm.es>
2021-03-26 12:14:59 +0100

    Creado el proyecto Common y en este, creado el componente Vector3
    Co-Authored-By: raulblas7 <48948743+raulblas7@users.noreply.github.com>

---
+++ Engine/src/Common/Vector3.cpp
@@ -0,0 +1,74 @@
+#include "Vector3.h"
+
+void Vector3::invert()
+{
+	x = -x;
+	y = -y;
+	z = -z;
+}
+
+float Vector3::magnitude() const
+{
+	return sqrt(x * x + y * y + z * z);
+}
+
+float Vector3::squareMagnitude() const
+{
+	return x * x + y * y + z * z;
+}
+
+void Vector3::operator*=(const float k)
+{
+	x *= k;
+	y *= k;
+	z = k;
+}
+
+Vector3 Vector3::operator*(const float k)
+{
+	return Vector3(x*k, y*k, z*k);
+}
+
+
+float Vector3::normalize()
+{
+	float m = magnitude();
+	*this *= 1.0f / m;
+	return m;
+}
+
+float Vector3::operator*(const Vector3& v) const
+{
+	return x * v.x + y * v.y + z * v.z;
+}
+
+float Vector3::dot(const Vector3& v) const
+{
+	return x*v.x + y*v.y + z* v.z;
+}
+
+Vector3 Vector3::operator+(const Vector3& other)
+{
+	return Vector3(x + other.x, y + other.y, z + other.z);
+}
+
+Vector3 Vector3::operator-(const Vector3& other)
+{
+	return Vector3(x - other.x, y - other.y, z - other.z);
+}
+
+void Vector3::operator-=(const Vector3& other)
+{
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+}
+
+Vector3 Vector3::operator%(const Vector3&)
+{
+	return Vector3(y*v.z - z*v.y, z*v.x - x*v.z, x*v.y - y*v.x);
+}
+
+
+
+
---
+++ Engine/src/Common/Vector3.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <math.h>
+
+class Vector3
+{
+public:
+	 float x; //Guarda el valor en el eje X
+	 float y; //Guarda el valor en el eje X
+	 float z; //Guarda el valor en el eje X
+private:
+	float pad; //
+public:
+	//Constructor por defecto(todos los valores a cero
+	Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+	//Constructor
+	Vector3(const float x1, const float y1, const float z1): x(x1),y(y1),z(z1){}
+
+	//Invertir todos los valores del vector
+	void invert();
+	// Optener la magnitud del vector
+	float magnitude() const;
+	
+	//Obtener la magnitud del vector al cuadrado
+	float squareMagnitude() const;
+	
+	// Multiplicar el vector por un escalar
+	void operator*=(const float k);
+
+	//Devuelve la copia de un vector multiplicado por un escalar
+	Vector3 operator*(const float k);
+
+	//normalizar el vector
+	float normalize();
+
+	//producto escalar de dos vectores
+	float operator*(const Vector3& v) const;
+
+	// Funcion para el producto escalar
+	float dot(const Vector3& v) const;
+
+	// sumar dos vectores
+	Vector3 operator+(const Vector3& other);
+	
+	// Suma a tu vector otro
+	void operator+=(const Vector3& other);
+
+   // restar dos vectores
+	Vector3 operator-(const Vector3& other);
+
+	//restar a tu vector otro
+	void operator-=(const Vector3& other);
+
+	//producto vectorial
+	Vector3 operator%(const Vector3&);
+
+
+
+
+};
