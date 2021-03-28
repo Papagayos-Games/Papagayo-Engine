c30766bc065cd6f771ee22043cb5b56a235f524d
Pablo Rodriguez Bobada <prodra02@ucm.es>
2021-03-28 18:14:50 +0200

    MOD: Instrucciones.txt

--- bats/2_Bullet.bat
+++ bats/2_Bullet.bat
@@ -4,6 +4,6 @@ echo ---------------------------------------------
 echo Iniciando proyecto de Bullet generado en build. Compilar en Debug x64 y Release x64
 echo ---------------------------------------------
 
-start ..\Engine\dependencies\Bullet\build\BULLET-PHYSICS.sln
+start ..\Engine\dependencies\Bullet\build\BULLET_PHYSICS.sln
 
 PAUSE
