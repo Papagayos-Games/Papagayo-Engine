c96691d15fa1b8c246b3527384fec2290055dae2
aurora <aurgar01@ucm.es>
2021-04-12 17:26:00 +0200

    MOD: camera casi terminada(falta el update)
    Co-Authored-By: Leyre Osés Sánchez <48771457+leoses@users.noreply.github.com>

--- Engine/include/Graphics/Camera.h
+++ Engine/include/Graphics/Camera.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "Component.h"
+#include <string>
 
 class Vector3;
 
 namespace Ogre {
 	class Camera;
 	class SceneNode;
-	class TransformSpace;
+	
 }
 
 class Camera : public Component
@@ -15,16 +16,25 @@ protected:
 	//Nodo que contiene a esta camara
 	Ogre::SceneNode* camNode_ = nullptr;
 	Ogre::Camera* mCamera_ = nullptr;
-
+	Ogre::Viewport* vp_ = nullptr;
+	std::string name = "MainCamera";
 	virtual void init()override;
 public:
 	Camera();
+	Camera(std::string cameraName);
+	//camara unica de la escena
+	
 	virtual ~Camera();
 
 	virtual void update()override;
+	
 
 	void setCameraPosition(Vector3 newPos);
-	//void setCameraDir(Vector3 newDir, Ogre::Node::TransformSpace  transformSpace = Ogre::Node::TS_WORLD);
+	void setCameraDir(Vector3 newDir);
+	void setBackgroundColor(Vector3 newColor ,float alpha);
+	void setNearClipDistance(int distance);
+	void setFarClipDistance(int distance);
+
 
 	Vector3 getCameraPosition();
 	inline Ogre::Camera* getCamera();
--- Engine/src/Graphics/Camera.cpp
+++ Engine/src/Graphics/Camera.cpp
@@ -8,27 +8,32 @@
 
 void Camera::init()
 {
-	mCamera_ = OgreContext::getInstance()->getSceneManager()->createCamera("MainCamera");
+	mCamera_ = OgreContext::getInstance()->getSceneManager()->createCamera(name);
 	mCamera_->setNearClipDistance(1);
 	mCamera_->setFarClipDistance(10000);
 	//mCamera_->lookAt(0, 0, -1);
 	mCamera_->setAutoAspectRatio(true);
 	//cam->setPolygonMode(Ogre::PM_WIREFRAME); 
 
-	camNode_ = OgreContext::getInstance()->getSceneManager()->getRootSceneNode()->createChildSceneNode("mCam");
+	camNode_ = OgreContext::getInstance()->getSceneManager()->getRootSceneNode()->createChildSceneNode(name+"Node");
 	camNode_->attachObject(mCamera_);
 
 	camNode_->setPosition(0, 0, 1000);
 	camNode_->lookAt(Ogre::Vector3(0, 0, 0), Ogre::Node::TS_WORLD);
 
-	Ogre::Viewport* vp = WindowGenerator::getInstance()->getRenderWindow()->addViewport(mCamera_);
-	vp->setBackgroundColour(Ogre::ColourValue(0.0, 1.0, 0.0, 1.0));//cambia el color del fondo
+	vp_ = WindowGenerator::getInstance()->getRenderWindow()->addViewport(mCamera_);
+	vp_->setBackgroundColour(Ogre::ColourValue(0.0, 1.0, 0.0, 1.0));//cambia el color del fondo
 }
 
 Camera::Camera(): Component(ecs::CmpId::Camera)
 {
 	init();
 
+}
+
+Camera::Camera(std::string cameraName) : Component(ecs::CmpId::Camera),name(cameraName)
+{
+	init();
 }
 
 Camera::~Camera()
@@ -45,11 +50,28 @@ void Camera::setCameraPosition(Vector3 newPos)
 	camNode_->setPosition(Ogre::Vector3(newPos.x, newPos.y, newPos.z));
 }
 
-//void Camera::setCameraDir(Vector3 newDir, Ogre::Node::TransformSpace transformSpace)
-//{
-//	newDir.normalize();
-//	camNode_->lookAt(Ogre::Vector3(newDir.x, newDir.y, newDir.z), transformSpace);
-//}
+void Camera::setCameraDir(Vector3 newDir)
+{
+	newDir.normalize();
+	camNode_->lookAt(Ogre::Vector3(newDir.x, newDir.y, newDir.z), Ogre::Node::TS_WORLD);
+}
+
+void Camera::setBackgroundColor(Vector3 newColor, float alpha)
+{
+	vp_->setBackgroundColour(Ogre::ColourValue(newColor.x, newColor.y, newColor.z, alpha));//cambia el color del fondo
+}
+
+void Camera::setNearClipDistance(int distance)
+{
+	mCamera_->setNearClipDistance(distance);
+
+}
+
+void Camera::setFarClipDistance(int distance)
+{
+	
+	mCamera_->setFarClipDistance(distance);
+}
 
 Vector3 Camera::getCameraPosition()
 {
