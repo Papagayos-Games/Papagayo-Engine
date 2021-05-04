dd7dba7b37c45c1425835db1a6a8d821cc8b784f
Anass <anasca01@ucm.es>
2021-05-04 02:13:50 +0200

    intento de movimiento WASD

--- Engine/bin/LuaScripts/prueba.lua
+++ Engine/bin/LuaScripts/prueba.lua
@@ -22,7 +22,25 @@ end
 function pressKeyDoSomething(input,rig)
 	if input:keyPressed(4) == true then
 	print("Pulse A perros");
-	 vec= Vector3(100,100,1000);
+	 vec= Vector3(-10,0,0);
+    rel= Vector3(0,0,0);
+	rig:addForce1(vec,rel,1);
+	end
+	if input:keyPressed(22) == true then
+	print("Pulse S perros");
+	 vec= Vector3(0,-10,0);
+    rel= Vector3(0,0,0);
+	rig:addForce1(vec,rel,1);
+	end
+	if input:keyPressed(26) == true then
+	print("Pulse W perros");
+	 vec= Vector3(0,10,0);
+    rel= Vector3(0,0,0);
+	rig:addForce1(vec,rel,1);
+	end
+	if input:keyPressed(7) == true then
+	print("Pulse D perros");
+	 vec= Vector3(10,0,0);
     rel= Vector3(0,0,0);
 	rig:addForce1(vec,rel,1);
 	end
