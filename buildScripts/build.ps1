9d6682d9063986fff8c1c942dbaa31ac4cd0d9c6
Stiven <starias@ucm.es>
2021-05-18 10:19:59 +0200

    Me colé v2

--- buildScripts/build.ps1
+++ buildScripts/build.ps1
@@ -142,135 +142,135 @@ $local:CMake = Find-CMake -CMakePath $CMakePath
 
 Try {
     # Build FMod
-   #If ($BuildFmod) {
-   #    Step-CopyToFolder -To $BinaryDirectory -From "fmod" -Paths @(
-   #        "$FModFolder\fmod.dll",
-	#		"$FModFolder\fmodL.dll",
-	#		"$FModFolder\lib\fmod_vc.lib",
-	#		"$FModFolder\lib\fmodL_vc.lib"
-   #    )
-   #}
-	#
-   ## Build LUA
-   #If ($BuildLUA) {
-   #    Step-CopyToFolder -To $BinaryDirectory -From "lua" -Paths @(
-   #        "$LUAFolder\lua54.dll",
-	#		"$LUAFolder\liblua54.a"
-   #    )
-   #}
-	#
-	## Build Ogre
-   #If ($BuildOgre) {
-   #    Step-CMake $CMake $OgreFolder @(
-	#	"-DOGRE_BUILD_COMPONENT_OVERLAY:BOOL=OFF",
-	#	"-DOGRE_BUILD_COMPONENT_BITES:BOOL=OFF"
-	#	)
-	#
-   #    If ($NDebug) {
-   #        Step-VisualStudio -MsBuild $MsBuild -Path "$OgreFolder\build\OGRE.sln" -Configuration "Debug"
-   #        Step-CopyToFolder -To $BinaryDirectory -From "Ogre" -Paths @(
-   #            "$OgreFolder\build\bin\debug\OgreMain_d.dll",
-   #            "$OgreFolder\build\bin\debug\RenderSystem_Direct3D11_d.dll",
-   #            "$OgreFolder\build\bin\debug\RenderSystem_GL_d.dll",
-   #            "$OgreFolder\build\bin\debug\OgreRTShaderSystem_d.dll",
-   #            "$OgreFolder\build\bin\debug\Codec_STBI_d.dll",
-   #            "$OgreFolder\build\bin\debug\Plugin_ParticleFX_d.dll",
-   #            "$OgreFolder\build\bin\debug\zlib.dll",
-	#			"$OgreFolder\build\bin\debug\SDL2.dll",
-	#			"$OgreFolder\build\SDL2-build\RelWithDebInfo\SDL2.lib",
-	#			"$OgreFolder\build\lib\Debug\OgreMain_d.lib",
-	#			"$OgreFolder\build\lib\Debug\Codec_STBI_d.lib",
-	#			"$OgreFolder\build\lib\Debug\OgreRTShaderSystem_d.lib",
-	#			"$OgreFolder\build\lib\Debug\RenderSystem_Direct3D11_d.lib"
-   #        )
-   #    }
-	#
-   #    If ($NRelease) {
-   #        Step-VisualStudio -MsBuild $MsBuild -Path "$OgreFolder\build\OGRE.sln" -Configuration "Release"
-   #        Step-CopyToFolder -To $BinaryDirectory -From "Ogre" -Paths @(
-   #            "$OgreFolder\build\bin\release\OgreMain.dll",
-   #            "$OgreFolder\build\bin\release\RenderSystem_Direct3D11.dll",
-   #            "$OgreFolder\build\bin\release\RenderSystem_GL.dll",
-   #            "$OgreFolder\build\bin\release\OgreRTShaderSystem.dll",
-   #            "$OgreFolder\build\bin\release\Codec_STBI.dll",
-   #            "$OgreFolder\build\bin\release\Plugin_ParticleFX.dll",
-   #            "$OgreFolder\build\bin\release\zlib.dll"
-	#			"$OgreFolder\build\bin\release\SDL2.dll",
-	#			"$OgreFolder\build\SDL2-build\RelWithDebInfo\SDL2main.lib",
-	#			"$OgreFolder\build\lib\Release\OgreMain.lib",
-	#			"$OgreFolder\build\lib\Release\Codec_STBI.lib",
-	#			"$OgreFolder\build\lib\Release\OgreRTShaderSystem.lib",
-	#			"$OgreFolder\build\lib\Release\RenderSystem_Direct3D11.lib"
-   #        )
-   #    }
-   #}
-	#
-   ## Build Bullet
-   #If ($BuildBullet) {
-   #    Step-CMake $CMake $BulletFolder @(
-   #        "-DBUILD_BULLET2_DEMOS:BOOL=OFF",
-   #        "-DBUILD_BULLET3:BOOL=ON",
-   #        "-DBUILD_CLSOCKET:BOOL=OFF",
-   #        "-DBUILD_CPU_DEMOS:BOOL=OFF",
-   #        "-DBUILD_ENET:BOOL=OFF",
-   #        "-DBUILD_EXTRAS:BOOL=OFF",
-   #        "-DBUILD_OPENGL3_DEMOS:BOOL=OFF",
-   #        "-DBUILD_PYBULLET:BOOL=OFF",
-   #        "-DBUILD_SHARED_LIBS:BOOL=OFF",
-   #        "-DBUILD_UNIT_TESTS:BOOL=OFF",
-	#		"-DENABLE_VHACD:BOOL=OFF",
-	#		"-DINSTALL_LIBS:BOOL=ON",
-   #        "-DUSE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=ON"
-   #    )
-	#
-   #    If ($NDebug) {
-   #        Step-VisualStudio -MsBuild $MsBuild -Path "$BulletFolder\build\BULLET_PHYSICS.sln" -Configuration "Debug"
-	#		Step-CopyToFolder -To $BinaryDirectory -From "Bullet" -Paths @(
-   #            "$BulletFolder\\build\lib\Debug\BulletCollision_Debug.lib",
-	#			"$BulletFolder\\build\lib\Debug\BulletDynamics_Debug.lib",
-	#			"$BulletFolder\\build\lib\Debug\BulletInverseDynamics_Debug.lib",
-	#			"$BulletFolder\\build\lib\Debug\BulletSoftBody_Debug.lib",
-	#			"$BulletFolder\\build\lib\Debug\LinearMath_Debug.lib"
-   #        )
-   #    }
-	#
-   #    If ($NRelease) {
-   #        Step-VisualStudio -MsBuild $MsBuild -Path "$BulletFolder\build\BULLET_PHYSICS.sln" -Configuration "Release"
-	#		Step-CopyToFolder -To $BinaryDirectory -From "Bullet" -Paths @(
-	#			"$BulletFolder\\build\lib\Release\BulletCollision.lib",
-	#			"$BulletFolder\\build\lib\Release\BulletDynamics.lib",
-	#			"$BulletFolder\\build\lib\Release\BulletInverseDynamics.lib",
-	#			"$BulletFolder\\build\lib\Release\BulletSoftBody.lib",
-	#			"$BulletFolder\\build\lib\Release\LinearMath.lib"
-   #        )
-	#	}
-   #}
+    If ($BuildFmod) {
+        Step-CopyToFolder -To $BinaryDirectory -From "fmod" -Paths @(
+            "$FModFolder\fmod.dll",
+			"$FModFolder\fmodL.dll",
+			"$FModFolder\lib\fmod_vc.lib",
+			"$FModFolder\lib\fmodL_vc.lib"
+        )
+    }
+	
+    # Build LUA
+    If ($BuildLUA) {
+        Step-CopyToFolder -To $BinaryDirectory -From "lua" -Paths @(
+            "$LUAFolder\lua54.dll",
+			"$LUAFolder\liblua54.a"
+        )
+    }
+	
+	# Build Ogre
+    If ($BuildOgre) {
+        Step-CMake $CMake $OgreFolder @(
+		"-DOGRE_BUILD_COMPONENT_OVERLAY:BOOL=OFF",
+		"-DOGRE_BUILD_COMPONENT_BITES:BOOL=OFF"
+		)
+	
+        If ($NDebug) {
+            Step-VisualStudio -MsBuild $MsBuild -Path "$OgreFolder\build\OGRE.sln" -Configuration "Debug"
+            Step-CopyToFolder -To $BinaryDirectory -From "Ogre" -Paths @(
+                "$OgreFolder\build\bin\debug\OgreMain_d.dll",
+                "$OgreFolder\build\bin\debug\RenderSystem_Direct3D11_d.dll",
+                "$OgreFolder\build\bin\debug\RenderSystem_GL_d.dll",
+                "$OgreFolder\build\bin\debug\OgreRTShaderSystem_d.dll",
+                "$OgreFolder\build\bin\debug\Codec_STBI_d.dll",
+                "$OgreFolder\build\bin\debug\Plugin_ParticleFX_d.dll",
+                "$OgreFolder\build\bin\debug\zlib.dll",
+				"$OgreFolder\build\bin\debug\SDL2.dll",
+				"$OgreFolder\build\SDL2-build\RelWithDebInfo\SDL2.lib",
+				"$OgreFolder\build\lib\Debug\OgreMain_d.lib",
+				"$OgreFolder\build\lib\Debug\Codec_STBI_d.lib",
+				"$OgreFolder\build\lib\Debug\OgreRTShaderSystem_d.lib",
+				"$OgreFolder\build\lib\Debug\RenderSystem_Direct3D11_d.lib"
+            )
+        }
+	
+        If ($NRelease) {
+            Step-VisualStudio -MsBuild $MsBuild -Path "$OgreFolder\build\OGRE.sln" -Configuration "Release"
+            Step-CopyToFolder -To $BinaryDirectory -From "Ogre" -Paths @(
+                "$OgreFolder\build\bin\release\OgreMain.dll",
+                "$OgreFolder\build\bin\release\RenderSystem_Direct3D11.dll",
+                "$OgreFolder\build\bin\release\RenderSystem_GL.dll",
+                "$OgreFolder\build\bin\release\OgreRTShaderSystem.dll",
+                "$OgreFolder\build\bin\release\Codec_STBI.dll",
+                "$OgreFolder\build\bin\release\Plugin_ParticleFX.dll",
+                "$OgreFolder\build\bin\release\zlib.dll"
+				"$OgreFolder\build\bin\release\SDL2.dll",
+				"$OgreFolder\build\SDL2-build\RelWithDebInfo\SDL2main.lib",
+				"$OgreFolder\build\lib\Release\OgreMain.lib",
+				"$OgreFolder\build\lib\Release\Codec_STBI.lib",
+				"$OgreFolder\build\lib\Release\OgreRTShaderSystem.lib",
+				"$OgreFolder\build\lib\Release\RenderSystem_Direct3D11.lib"
+            )
+        }
+    }
+	
+    # Build Bullet
+    If ($BuildBullet) {
+        Step-CMake $CMake $BulletFolder @(
+            "-DBUILD_BULLET2_DEMOS:BOOL=OFF",
+            "-DBUILD_BULLET3:BOOL=ON",
+            "-DBUILD_CLSOCKET:BOOL=OFF",
+            "-DBUILD_CPU_DEMOS:BOOL=OFF",
+            "-DBUILD_ENET:BOOL=OFF",
+            "-DBUILD_EXTRAS:BOOL=OFF",
+            "-DBUILD_OPENGL3_DEMOS:BOOL=OFF",
+            "-DBUILD_PYBULLET:BOOL=OFF",
+            "-DBUILD_SHARED_LIBS:BOOL=OFF",
+            "-DBUILD_UNIT_TESTS:BOOL=OFF",
+			"-DENABLE_VHACD:BOOL=OFF",
+			"-DINSTALL_LIBS:BOOL=ON",
+            "-DUSE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=ON"
+        )
+	
+        If ($NDebug) {
+            Step-VisualStudio -MsBuild $MsBuild -Path "$BulletFolder\build\BULLET_PHYSICS.sln" -Configuration "Debug"
+			Step-CopyToFolder -To $BinaryDirectory -From "Bullet" -Paths @(
+                "$BulletFolder\\build\lib\Debug\BulletCollision_Debug.lib",
+				"$BulletFolder\\build\lib\Debug\BulletDynamics_Debug.lib",
+				"$BulletFolder\\build\lib\Debug\BulletInverseDynamics_Debug.lib",
+				"$BulletFolder\\build\lib\Debug\BulletSoftBody_Debug.lib",
+				"$BulletFolder\\build\lib\Debug\LinearMath_Debug.lib"
+            )
+        }
+	
+        If ($NRelease) {
+            Step-VisualStudio -MsBuild $MsBuild -Path "$BulletFolder\build\BULLET_PHYSICS.sln" -Configuration "Release"
+			Step-CopyToFolder -To $BinaryDirectory -From "Bullet" -Paths @(
+				"$BulletFolder\\build\lib\Release\BulletCollision.lib",
+				"$BulletFolder\\build\lib\Release\BulletDynamics.lib",
+				"$BulletFolder\\build\lib\Release\BulletInverseDynamics.lib",
+				"$BulletFolder\\build\lib\Release\BulletSoftBody.lib",
+				"$BulletFolder\\build\lib\Release\LinearMath.lib"
+            )
+		}
+    }
 	
 	
     # Build CEGUI's dependencies
-    #If ($BuildCeguiDependencies) {
-    #    Step-CMake $CMake $CeguiDependenciesFolder @(
-	#		"-DCEGUI_BUILD_DEVIL:BOOL=ON",
-	#		"-DCEGUI_BUILD_EXPAT:BOOL=ON",
-	#		"-DCEGUI_BUILD_FREETYPE2:BOOL=ON",
-	#		"-DCEGUI_BUILD_FREEIMAGE:BOOL=ON",
-	#		"-DCEGUI_BUILD_GLEW:BOOL=ON",
-	#		"-DCEGUI_BUILD_GLFW:BOOL=ON",
-	#		"-DCEGUI_BUILD_GLM:BOOL=ON",
-	#		"-DCEGUI_BUILD_LUA:BOOL=ON",
-	#		"-DCEGUI_BUILD_PCRE:BOOL=ON",
-	#		"-DCEGUI_BUILD_SILLY:BOOL=ON"
-	#	)
-	#
-    #    If ($NDebug) {
-    #        Step-VisualStudio -MsBuild $MsBuild -Path "$CeguiDependenciesFolder\build\CEGUI-DEPS.sln" -Configuration "Debug"
-    #    }
-	#
-    #    If ($NRelease) {
-    #        Step-VisualStudio -MsBuild $MsBuild -Path "$CeguiDependenciesFolder\build\CEGUI-DEPS.sln" -Configuration "Release"
-    #    }
-	#	
-    #}
+    If ($BuildCeguiDependencies) {
+        Step-CMake $CMake $CeguiDependenciesFolder @(
+			"-DCEGUI_BUILD_DEVIL:BOOL=ON",
+			"-DCEGUI_BUILD_EXPAT:BOOL=ON",
+			"-DCEGUI_BUILD_FREETYPE2:BOOL=ON",
+			"-DCEGUI_BUILD_FREEIMAGE:BOOL=ON",
+			"-DCEGUI_BUILD_GLEW:BOOL=ON",
+			"-DCEGUI_BUILD_GLFW:BOOL=ON",
+			"-DCEGUI_BUILD_GLM:BOOL=ON",
+			"-DCEGUI_BUILD_LUA:BOOL=ON",
+			"-DCEGUI_BUILD_PCRE:BOOL=ON",
+			"-DCEGUI_BUILD_SILLY:BOOL=ON"
+		)
+	
+        If ($NDebug) {
+            Step-VisualStudio -MsBuild $MsBuild -Path "$CeguiDependenciesFolder\build\CEGUI-DEPS.sln" -Configuration "Debug"
+        }
+	
+        If ($NRelease) {
+            Step-VisualStudio -MsBuild $MsBuild -Path "$CeguiDependenciesFolder\build\CEGUI-DEPS.sln" -Configuration "Release"
+        }
+		
+    }
 
     # Build CEGUI
     If ($BuildCegui) {
@@ -302,8 +302,8 @@ Try {
 			"-DOGRE_LIB_DBG:FILEPATH=$OgreFolder/build/lib/Debug/OgreMain_d.lib"
 		)
 		
-         #Let's be honest, I got done with MSBuild here. The world would be beautiful if it'd let me define constants
-         #via CLI, but looks like it's impossible without CMake.
+        # Let's be honest, I got done with MSBuild here. The world would be beautiful if it'd let me define constants
+        # via CLI, but looks like it's impossible without CMake.
         $private:Content = Get-Content -Path "$CeguiFolder\build\cegui\include\CEGUI\Config.h"
         $private:Content = $Content -replace "define CEGUI_OGRE_VERSION_MAJOR 0", "define CEGUI_OGRE_VERSION_MAJOR 1"
         $private:Content = $Content -replace "define CEGUI_OGRE_VERSION_MINOR 0", "define CEGUI_OGRE_VERSION_MINOR 12"
